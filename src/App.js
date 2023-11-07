230e4c4154290fcc504984a6daeb81cd7f79dc6e
dhruvmehta2003 <dhruv2003mehta@gmail.com>
2023-11-07 22:34:59 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar/nav'
+import SignIn from './components/Login/SignIn';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <SignIn />
       </BrowserRouter>
     </>
   );
